9ee72a15471561836e69df965cd03fc3abaa7bf8
Adriano-Ivan <adrianoivandepaulacampos@gmail.com.br>
2022-02-23 14:42:51 -0300

    Lendo arquivo de forma assíncrona, tratando erro de leitura e utilizando regex para encontrar endereços de artigos no arquivo.

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const chalk = require("chalk");
 const fs = require("fs");
 
+function extrairLinks(texto) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const arrayResultados = [];
+
+  let temp;
+  while ((temp = regex.exec(texto)) !== null) {
+    arrayResultados.push({ [temp[1]]: temp[2] });
+  }
+
+  return arrayResultados;
+}
+
 function tratarErro(erro) {
   throw new Error(
     chalk.red(
@@ -9,14 +21,17 @@ function tratarErro(erro) {
     )
   );
 }
-function pegarArquivo(caminhoDoArquivo) {
-  const encoding = "utf-8";
-  fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-    if (erro) {
-      tratarErro(erro);
-    }
-    console.log(chalk.green(texto));
-  });
+
+async function pegarArquivo(caminhoDoArquivo) {
+  try {
+    const encoding = "utf-8";
+    const conteudo = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    console.log(extrairLinks(conteudo));
+  } catch (erro) {
+    tratarErro(erro);
+  } finally {
+    console.log(chalk.green("Fim da execução."));
+  }
 }
 
-pegarArquivo("./arquivos/text1.md");
+pegarArquivo("./arquivos/texto1.md");
